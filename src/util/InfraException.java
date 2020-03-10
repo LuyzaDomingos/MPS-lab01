992377d276989f705c72032b3d04a2238217e839
Luyza Domingos <luyzadomingos@cc.ci.ufpb.br>
2020-03-10 01:22:57 -0300

    Métodos UserPersistence

---
+++ src/util/InfraException.java
@@ -0,0 +1,16 @@
+package infra;
+
+/**
+ *
+ * @author Luyza
+ */
+public class InfraException extends Exception {
+
+    public InfraException(String message) {
+        super(message);
+    }
+
+    public InfraException(){
+        super("Erro na aplicação. Procure pelo o.....")
+    }
+}
